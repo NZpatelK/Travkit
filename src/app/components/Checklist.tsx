8a3c8a820188e08decb885f88d48d88c034eb9fd
NZpatelK <patelk.dev@hotmail.com>
2025-08-05 19:34:43 +1200

    set up title

---
+++ src/app/components/Checklist.tsx
@@ -0,0 +1,9 @@
+
+export default function Checklist() {
+    return (
+        <div className="flex flex-col max-w-2xl w-full bg-neutral-100 h-full max-h-200 rounded-2xl shadow-2xl">
+            <h3 className="text-lg font-semibold text-neutral-800 mt-5 ml-5 capitalize">Prepare trip to Dubai in 2025</h3>
+
+        </div>
+    )
+}
