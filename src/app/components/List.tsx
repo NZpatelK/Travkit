dfdf3c83df5b1108b3cdcbf696b1291ef9ad06d5
NZpatelK <patelk.dev@hotmail.com>
2025-08-06 21:49:45 +1200

    Update List.tsx

--- src/app/components/List.tsx
+++ src/app/components/List.tsx
@@ -4,6 +4,7 @@ export default function List() {
     return (
         <div className=" mx-auto my-4 border-2 border-neutral-200 mb-2 overflow-hidden w-full 0 h-10 rounded flex items-center p-2 ">
             <Checkbox/>
+            <span className="ml-4 text-neutral-700 line-through">Item 1</span>
 
         </div>
     );
