53b63dddc7a4372b322bd18ea97df3c25deec37f
NZpatelK <patelk.dev@hotmail.com>
2025-08-07 17:48:15 +1200

    add

---
+++ src/app/utils/supabase/server.ts
@@ -0,0 +1,11 @@
+// lib/supabase/server.ts
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export const createSupabaseServerClient = () => {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    { cookies }
+  )
+}
