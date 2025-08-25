e4b17bfd50465078817f404d3213b614790b0598
NZpatelK <patelk.dev@hotmail.com>
2025-08-25 15:47:59 +1200

    create template card

--- src/app/components/TemplateCard.tsx
+++ src/app/components/TemplateCard.tsx
@@ -1,10 +1,12 @@
 import { Category } from "../data/travelData";
+import Checkbox from "./Checkbox";
 
 
 export default function TemplateCard(category: Category) {
     return (
-        <div className="w-full h-full flex flex-col justify-center items-center">
-            <h2 className="text-2xl font-bold text-amber-200">{category.title}</h2>
+        <div className="w-full h-full flex items-center border-violet-700 bg-violet-100 border-2  shadow-md rounded p-2 m-2">
+            <Checkbox isChecked={false}/>
+            <h2 className="text-md ml-2 font-bold text-neutral-900 capitalize">{category.title}</h2>
         </div>
     );
 }
