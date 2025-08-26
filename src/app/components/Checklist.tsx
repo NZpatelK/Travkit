881a0c6cede4799b8476eab974c1f21844ce557d
NZpatelK <patelk.dev@hotmail.com>
2025-08-26 18:27:30 +1200

    update image

--- src/app/components/Checklist.tsx
+++ src/app/components/Checklist.tsx
@@ -26,18 +26,21 @@ export default function Checklist() {
 
     return (
         <div className="flex flex-col max-w-2xl w-full bg-neutral-100 min-h-[600px] rounded-2xl shadow-2xl m-5">
-            <div className="relative h-35 overlay">
+            <div className="relative h-50 overlay">
                 <Image
-                    src="/bg-image.jpg"
+                    src="/travel.jpg"
                     alt="Header Background Image"
                     fill
                     style={{ objectFit: 'cover' }}
                     className="rounded-t-2xl"
                 />
 
-                <h3 className="absolute bottom-8 left-5 text-2xl font-extrabold capitalize text-gray-800 z-20 drop-shadow-md">
-                    Prepare trip to Dubai in 2025
-                </h3>
+                <div>
+                    <h3 className="absolute bottom-11 left-5 text-2xl font-extrabold capitalize text-gray-800 z-20 drop-shadow-md">
+                        Dubai Trip Checklist
+                    </h3>
+                    <p className="text-neutral-900 text-sm z-20 absolute bottom-7 left-5">Duration: 5 days</p>
+                </div>
                 <div className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 z-20 w-10/12 sm:w-3/5 transition-all">
                     <ProgressBar progressNum={totalCompletedLists} />
                 </div>
